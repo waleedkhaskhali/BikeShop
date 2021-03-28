43777c18c9f1b76289d0bb9685d134ab503aca03
waleedkhaskhali <76614771+waleedkhaskhali@users.noreply.github.com>
2021-03-27 23:22:19 -0400

    Create Product.js

---
+++ src/components/Product.js
@@ -0,0 +1,30 @@
+import "./product.css";
+import React from "react";
+
+function Product() {
+  return (
+    <div>
+      <section className="product-container">
+        <div className="image-container">
+          <img
+            className="product-image"
+            src="https://i.pinimg.com/originals/c4/9d/16/c49d16317c5e577ac701b5aa85a15f2a.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div className="product">
+          <h1 className="product-name">Fixed Gear Bike</h1>
+          <hr></hr>
+          <h2 className="price-tag">29.99</h2>
+          <p className="product-description">
+            Our New Limited Time Fixies available Now
+          </p>
+          <button>Add To Cart</button>
+        </div>
+      </section>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default Product;
